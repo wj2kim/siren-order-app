bc9962c000efa50e0e005e7f77e508710a8b17cf
김우정 <wjkim@rsupport.com>
2020-09-11 23:34:26 +0900

    will be working on login

--- client/src/app/containers/SignIn/index.js
+++ client/src/app/containers/SignIn/index.js
@@ -5,7 +5,13 @@ import React from 'react'
 
 const SignIn = () => {
     return (
-   
+        <Wrapper>
+            <form onSubmit={handleSubmit}>
+                    <input type="email" name="email" placeholder="이메일" onChange={handleChange("email")} />
+                    <input type="password" name="email" placeholder= "비밀번호" onChange={handleChange("password")}/>
+                    <button type="submit">{loading ? '로딩중...' : '로그인'}</button>
+            </form>
+        </Wrapper>
     )
 }
 
